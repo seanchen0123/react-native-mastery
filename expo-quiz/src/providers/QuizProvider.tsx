7c4e1c902922c36371cd541775f9272381d135ce
seanchen0123 <270061968@qq.com>
2024-12-05 12:02:19 +0800

    feat:add implement data persist for saveing best score

--- expo-quiz/src/providers/QuizProvider.tsx
+++ expo-quiz/src/providers/QuizProvider.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren, useContext, useEffect, useState } from 'react'
 import { createContext } from 'react'
 import questions from '../questions'
 import { Question } from '../types'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 type QuizContext = {
   question?: Question
@@ -31,10 +32,15 @@ export default function QuizProvider({ children }: PropsWithChildren) {
   const [score, setScore] = useState(0)
   const [bestScore, setBestScore] = useState(0)
   const isFinished = questionIndex >= questions.length
+
+  useEffect(() => {
+    loadBestScore()
+  }, [])
   
   useEffect(() => {
     if (isFinished && score > bestScore) {
       setBestScore(score)
+      saveBestScore(score)
     }
   }, [isFinished])
 
@@ -54,6 +60,25 @@ export default function QuizProvider({ children }: PropsWithChildren) {
       setScore(prev => prev + 1)
     }
     setQuestionIndex(prev => prev + 1)
+  }
+
+  const saveBestScore = async (val: number) => {
+    try {
+      await AsyncStorage.setItem('bestScore', val.toString())
+    } catch (error) {
+      console.log('save best score error', error)
+    }
+  }
+
+  const loadBestScore = async () => {
+    try {
+      const bestScore = await AsyncStorage.getItem('bestScore')
+      if (bestScore !== null) {
+        setBestScore(parseInt(bestScore))
+      }
+    } catch (error) {
+      console.log('load best score error', error)
+    }
   }
 
   return (
