b84f838357768566a62bb1b95d60c36a3a60221a
seanchen0123 <270061968@qq.com>
2024-12-06 10:05:32 +0800

    feat: implement media file saving to photo gallery

--- expo-camera/src/app/image/[name].tsx
+++ expo-camera/src/app/image/[name].tsx
@@ -1,13 +1,15 @@
 import { Link, router, Stack, useLocalSearchParams } from 'expo-router'
-import { Image, Text, View } from 'react-native'
+import { Alert, Image, StyleSheet, Text, View } from 'react-native'
 import * as FileSystem from 'expo-file-system'
 import { MaterialIcons } from '@expo/vector-icons'
 import { getMediaType } from '../../utils/media'
 import { ResizeMode, Video } from 'expo-av'
 import { VideoView, useVideoPlayer } from 'expo-video'
+import * as MediaLibrary from 'expo-media-library'
 
 export default function ImageScreen() {
   const { name } = useLocalSearchParams()
+  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions()
 
   const fullUri = (FileSystem.documentDirectory || '') + (name || '')
   const type = getMediaType(fullUri)
@@ -22,23 +24,51 @@ export default function ImageScreen() {
     router.back()
   }
 
-  const onSave = async () => {}
+  const onSave = async () => {
+    try {
+      // 权限检查
+      const { status } = await MediaLibrary.requestPermissionsAsync()
+      if (status !== 'granted') {
+        Alert.alert('需要权限', '保存图片需要访问相册权限', [{ text: '确定' }])
+        return null
+      }
+      const asset = await MediaLibrary.createAssetAsync(fullUri)
+      if (asset) {
+        Alert.alert('成功', '图片已保存到相册')
+      }
+    } catch (error) {
+      console.error('保存图片失败:', error)
+    }
+  }
 
   return (
     <View>
       <Stack.Screen
         options={{
-          title: 'Media',
-          headerRight: () => (
-            <View style={{ flexDirection: 'row', gap: 6 }}>
-              <MaterialIcons name="delete" size={26} color={'crimson'} onPress={onDelete} />
-              <MaterialIcons name="save" size={26} color={'dimgray'} onPress={onSave} />
-            </View>
-          )
+          title: 'Media'
         }}
       />
       {type === 'image' && <Image source={{ uri: fullUri }} style={{ width: '100%', height: '100%' }} />}
       {type === 'video' && <VideoView player={player} style={{ width: '100%', height: '100%' }} contentFit="cover" />}
+      <View style={styles.btnArea}>
+        <MaterialIcons name="delete" size={30} color={'#fb7185'} onPress={onDelete} />
+        <MaterialIcons name="save" size={30} color={'#60a5fa'} onPress={onSave} />
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  btnArea: {
+    padding: 12,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    gap: 20,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    backgroundColor: 'rgba(0,0,0,0.3)'
+  }
+})
