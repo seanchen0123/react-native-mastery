e7d8e53755e0311c12152667493788fbe4812330
seanchen0123 <270061968@qq.com>
2024-12-09 10:36:35 +0800

    feat: implement personal form with react-hook-form and zod validation

--- expo-forms/src/app/checkout/personal.tsx
+++ expo-forms/src/app/checkout/personal.tsx
@@ -1,40 +1,44 @@
-import {
-  StyleSheet,
-  View,
-} from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { router } from 'expo-router'
 import CustomButton from '../../components/CustomButton'
 import CustomTextInput from '../../components/CustomTextInput'
 import KeyboardAwareScrollView from '../../components/KeyboardAwareScrollView'
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const PersonalInfoSchema = z.object({
+  fullName: z.string({ message: 'Full name is required'}).min(3, {message: 'Full name must be at least 3 character long'}).trim(),
+  address: z.string({message: 'Please provide your address'}).trim(),
+  city: z.string({ message: 'city is required'}).trim(),
+  postcode: z.string({message: 'postcode is required'}),
+  phone: z.string({ message: 'phone number is required'})
+})
+
+type PersonalInfo = z.infer<typeof PersonalInfoSchema>
 
 export default function PersonalDetailForm() {
-  const onNext = () => {
+  const form = useForm<PersonalInfo>({
+    resolver: zodResolver(PersonalInfoSchema)
+  })
+
+  const onNext: SubmitHandler<PersonalInfo> = data => {
+    console.log(data)
     router.push('/checkout/payment')
   }
 
   return (
     <KeyboardAwareScrollView>
-      <CustomTextInput label="Full name" placeholder="Joe do" />
-      <CustomTextInput label="Address" placeholder="Address" />
-      <CustomTextInput label="Address" placeholder="Address" />
-      <CustomTextInput label="Address" placeholder="Address" />
-      <CustomTextInput label="Address" placeholder="Address" />
-      <CustomTextInput label="Address" placeholder="Address" />
-      <CustomTextInput label="Address" placeholder="Address" />
-      <View style={{ flexDirection: 'row', gap: 6 }}>
-        <CustomTextInput
-          label="City"
-          placeholder="Wuhan"
-          containerStyle={{ flex: 1 }}
-        />
-        <CustomTextInput
-          label="Post cde"
-          placeholder="420000"
-          containerStyle={{ flex: 1 }}
-        />
-      </View>
-      <CustomTextInput label="Phone number" placeholder="13312345678" />
-      <CustomButton title="Next" style={styles.button} onPress={onNext} />
+      <FormProvider {...form}>
+        <CustomTextInput name="fullName" label="Full name" placeholder="Joe do" />
+        <CustomTextInput name="address" label="Address" placeholder="Address" />
+        <View style={{ flexDirection: 'row', gap: 6 }}>
+          <CustomTextInput name="city" label="City" placeholder="Wuhan" containerStyle={{ flex: 1 }} />
+          <CustomTextInput name="postcode" label="Post cde" placeholder="420000" containerStyle={{ flex: 1 }} />
+        </View>
+        <CustomTextInput name="phone" label="Phone number" placeholder="13312345678" />
+        <CustomButton title="Next" style={styles.button} onPress={form.handleSubmit(onNext)} />
+      </FormProvider>
     </KeyboardAwareScrollView>
   )
 }
--- expo-forms/src/components/CustomTextInput.tsx
+++ expo-forms/src/components/CustomTextInput.tsx
@@ -1,16 +1,32 @@
 import { ComponentProps } from 'react'
+import { useController } from 'react-hook-form'
 import { StyleProp, StyleSheet, Text, TextInput, View, ViewStyle } from 'react-native'
 
-type CustomTextInput = { label?: string; containerStyle?: StyleProp<ViewStyle> } & ComponentProps<typeof TextInput>
+type CustomTextInput = { label?: string; containerStyle?: StyleProp<ViewStyle>; name: string } & ComponentProps<
+  typeof TextInput
+>
 
-export default function CustomTextInput({ label, containerStyle, ...textInputProps }: CustomTextInput) {
-  const error: {message: string} = { message: '' } 
+export default function CustomTextInput({ label, containerStyle, name, ...textInputProps }: CustomTextInput) {
+  const {
+    field: { value, onChange, onBlur },
+    fieldState: { error }
+  } = useController({ name })
 
   return (
     <View style={containerStyle}>
       {label && <Text style={styles.label}>{label}</Text>}
-      <TextInput {...textInputProps} style={[styles.input, textInputProps.style, error ? styles.errorInput : {}]} />
-      {error && error.message && <Text style={styles.error} numberOfLines={1}>{error.message}</Text>}
+      <TextInput
+        {...textInputProps}
+        value={value}
+        onBlur={onBlur}
+        onChangeText={onChange}
+        style={[styles.input, textInputProps.style, error ? styles.errorInput : {}]}
+      />
+      {error && error.message && (
+        <Text style={styles.error} numberOfLines={1}>
+          {error.message}
+        </Text>
+      )}
     </View>
   )
 }
