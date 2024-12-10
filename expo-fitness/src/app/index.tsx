aa64cc0bb067db1aedf2016b2586249aa8a77428
seanchen0123 <270061968@qq.com>
2024-12-10 09:12:38 +0800

    expo-fitness project init

---
+++ expo-fitness/src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+import '../../global.css'
+
+export default function App() {
+  return (
+    <View className="bg-red-500 flex-1">
+      <Text>hello world</Text>
+    </View>
+  )
+}
